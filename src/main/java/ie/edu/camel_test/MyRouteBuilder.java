48f79990dfaa3b794e06d7ba52a0885c07f9d46f
Bruno Caú <brunoalcau@gmail.com>
2018-02-22 18:59:06 -0300

    Test camel csv

---
+++ src/main/java/ie/edu/camel_test/MyRouteBuilder.java
@@ -0,0 +1,23 @@
+package ie.edu.camel_test;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        // here is a sample which processes the input files
+        // (leaving them in place - see the 'noop' flag)
+        // then performs content based routing on the message using XPath
+        from("file:src/data?noop=true")
+        	.log("${body}")
+            .to("file:target/messages/uk");
+    }
+
+}
